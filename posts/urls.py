db595cf378f01caf6ae44ced62a9c0094b7e841e
Pedro Miguel <pedro.miguel@live.co.uk>
2018-02-16 14:16:13 +0000

    Adds functionality to delete posts.

--- posts/urls.py
+++ posts/urls.py
@@ -7,9 +7,9 @@ from .views import (
 
 
 urlpatterns = [
-    url(r'^list/$', post_list),
+    url(r'^list/$', post_list, name='list'),
     url(r'^create/$', post_create),
     url(r'^(?P<id>\d+)/$', post_detail, name='detail'),
     url(r'^(?P<id>\d+)/edit/$', post_update, name='update'),
-    url(r'^delete/$', post_delete),
+    url(r'^(?P<id>\d+)/delete/$', post_delete, name='delete'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import PostForm
 from .models import Post
@@ -56,5 +56,8 @@ def post_update(request, id=None):
     return render(request, 'post_form.html', context)
 
 
-def post_delete(request):
-    return HttpResponse('<h1>Hello</h1>')
+def post_delete(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    instance.delete()
+    messages.success(request, 'Post Deleted Successfully', extra_tags='some-tag')
+    return redirect("posts:list")
