ec7a88aa92c6873a59972df9a5858124df2bb1e7
Pedro Miguel <pedro.miguel@live.co.uk>
2018-02-16 13:36:27 +0000

    Adds functionality to update posts.

--- posts/urls.py
+++ posts/urls.py
@@ -7,9 +7,9 @@ from .views import (
 
 
 urlpatterns = [
+    url(r'^list/$', post_list),
     url(r'^create/$', post_create),
     url(r'^(?P<id>\d+)/$', post_detail, name='detail'),
-    url(r'^list/$', post_list),
-    url(r'^update/$', post_update),
+    url(r'^(?P<id>\d+)/edit/$', post_update, name='update'),
     url(r'^delete/$', post_delete),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
 from .forms import PostForm
@@ -11,6 +11,7 @@ def post_create(request):
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
+        return HttpResponseRedirect(instance.get_absolute_url())
 
     context = {
         'form': form,
@@ -34,8 +35,21 @@ def post_list(request):
     return render(request, 'index.html', context)
 
 
-def post_update(request):
-    return HttpResponse('<h1>Update</h1>')
+def post_update(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    form = PostForm(request.POST or None, instance=instance)
+
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        return HttpResponseRedirect(instance.get_absolute_url())
+
+    context = {
+        'title': instance.title,
+        'instance': instance,
+        'form': form,
+    }
+    return render(request, 'post_form.html', context)
 
 
 def post_delete(request):
