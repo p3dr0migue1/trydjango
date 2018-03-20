f12437e57acf55a0d072b78a303c896919a2e341
Pedro Miguel <pedro.miguel@live.co.uk>
2018-03-20 14:30:55 +0000

    Adds generic foreign keys and comments app.

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,12 @@
 from django.contrib import messages
+from django.contrib.contenttypes.models import ContentType
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Q
 from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
+from comments.models import Comment
 from .forms import PostForm
 from .models import Post
 
@@ -34,9 +36,14 @@ def post_detail(request, slug):
         if not request.user.is_staff or not request.user.is_superuser:
             raise Http404
 
+    content_type = ContentType.objects.get_for_model(Post)
+    obj_id = instance.id
+    comments = Comment.objects.filter(content_type=content_type, object_id=obj_id)
+
     context = {
         'title': instance.title,
-        'instance': instance
+        'instance': instance,
+        'comments': comments,
     }
     return render(request, 'post_detail.html', context)
 
