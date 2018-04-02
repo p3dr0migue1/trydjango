f92a555bffe13400f0848a668d0610e3471576f7
Pedro Miguel <pedro.miguel@live.co.uk>
2018-04-02 19:34:06 +0100

    Adds functionality to display read time when viewing posts

--- posts/models.py
+++ posts/models.py
@@ -11,6 +11,7 @@ from django.utils.safestring import mark_safe
 from markdown_deux import markdown
 
 from comments.models import Comment
+from .utils import get_read_time
 
 
 class PostManager(models.Manager):
@@ -39,6 +40,7 @@ class Post(models.Model):
     content = models.TextField()
     draft = models.BooleanField(default=False)
     publish = models.DateField(auto_now=False, auto_now_add=False)
+    read_time = models.IntegerField(default=0) 
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
 
@@ -91,5 +93,9 @@ def pre_save_post_receiver(sender, instance, *args, **kwargs):
     if not instance.slug:
         instance.slug = create_slug(instance)
 
+    html_string = instance.get_markdown()
+    read_time = get_read_time(html_string)
+    instance.read_time = read_time
 
 pre_save.connect(pre_save_post_receiver, sender=Post)
+    
