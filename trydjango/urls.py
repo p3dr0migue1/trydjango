7ec31e2fe490c53719933b58d9bd1dae900c7106
Pedro Miguel <pedro.miguel@live.co.uk>
2018-08-16 08:19:33 +0100

    Update urls to include the comments api

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     url(r'^register/', register_view, name='register'),
     url(r'^comments/', include('comments.urls', namespace='comments')),
     url(r'^api/posts/', include('posts.api.urls', namespace='posts-api')),
+    url(r'^api/comments/', include('comments.api.urls', namespace='comments-api')),
     url(r'^', include('posts.urls', namespace='posts')),
 ]
 
